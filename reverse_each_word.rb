be98daefc5b418f4157f04173ccfc1538c791df9
elegant-intelligence-1985 <cp.halpenny@gmail.com>
2019-07-06 17:38:14 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,23 @@
+
+#.each method
+def reverse_each_word(sentence)
+  # take in an argument of a sentence
+  # convert to an array
+  sentence.split
+  # create new array to hold new data
+  reversed = []
+  # iterate across array using .each method to reverse each word
+  sentence.each do |
+  # convert back to a string
+  # call new array
+end
+
+#.collect method
+  # take in an argument of a sentence
+  def reverse_each_word_easy(sentence_string)
+  # convert to an array
+  sentence_string.split
+  # iterate across arra using .collect method to reverse each word
+
+  # convert back to a string
+end
